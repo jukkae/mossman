ae944af2023b2304cd9b2c8bcf45002cd4c7a822
Jukka Eerikäinen <jukka.eerikainen@aalto.fi>
2015-03-13 23:27:26 +0200

    change from static to dynamic library, get audio device probing working

--- mossman/src/core/Application.cpp
+++ mossman/src/core/Application.cpp
@@ -46,33 +46,99 @@ void Application::init() {
 
 	mSceneManager.addScene(golScene, golSceneRenderer);
 
-	RtAudio *audio = 0;
-	try {
-		audio = new RtAudio();
-	} catch (RtAudioError &error) {
-		error.printMessage();
-		std::cout << "Error initializing RtAudio";
-		exit(EXIT_FAILURE);
-	}
+	// TODO this is only here to get RtAudio up and running,
+	// refactor later!
 
-	// TODO this here only to test if RtAudio works at all, remove / move later
-	int devices = audio->getDeviceCount();
+	// Create an api map.
+	std::map<int, std::string> apiMap;
+	apiMap[RtAudio::MACOSX_CORE] = "OS-X Core Audio";
+	apiMap[RtAudio::WINDOWS_ASIO] = "Windows ASIO";
+	apiMap[RtAudio::WINDOWS_DS] = "Windows Direct Sound";
+	apiMap[RtAudio::WINDOWS_WASAPI] = "Windows WASAPI";
+	apiMap[RtAudio::UNIX_JACK] = "Jack Client";
+	apiMap[RtAudio::LINUX_ALSA] = "Linux ALSA";
+	apiMap[RtAudio::LINUX_PULSE] = "Linux PulseAudio";
+	apiMap[RtAudio::LINUX_OSS] = "Linux OSS";
+	apiMap[RtAudio::RTAUDIO_DUMMY] = "RtAudio Dummy";
 
+	std::vector<RtAudio::Api> apis;
+	RtAudio::getCompiledApi(apis);
+
+	std::cout << "\nRtAudio Version " << RtAudio::getVersion() << std::endl;
+
+	std::cout << "\nCompiled APIs:\n";
+	for (unsigned int i = 0; i < apis.size(); i++)
+		std::cout << "  " << apiMap[apis[i]] << std::endl;
+
+	RtAudio audio;
 	RtAudio::DeviceInfo info;
-	  for (int i=1; i<=devices; i++) {
 
-	    try {
-	      info = audio->getDeviceInfo(i);
-	    }
-	    catch (RtAudioError &error) {
-	      error.printMessage();
-	      break;
-	    }
+	std::cout << "\nCurrent API: " << apiMap[audio.getCurrentApi()]
+			<< std::endl;
 
-	    // max number of output channels for each device
-	    std::cout << "device = " << i;
-	    std::cout << ": maximum output channels = " << info.outputChannels << "\n";
-	  }
+	unsigned int devices = audio.getDeviceCount();
+	std::cout << "\nFound " << devices << " device(s) ...\n";
+
+	for (unsigned int i = 0; i < devices; i++) {
+		info = audio.getDeviceInfo(i);
+
+		std::cout << "\nDevice Name = " << info.name << '\n';
+		if (info.probed == false)
+			std::cout << "Probe Status = UNsuccessful\n";
+		else {
+			std::cout << "Probe Status = Successful\n";
+			std::cout << "Output Channels = " << info.outputChannels << '\n';
+			std::cout << "Input Channels = " << info.inputChannels << '\n';
+			std::cout << "Duplex Channels = " << info.duplexChannels << '\n';
+			if (info.isDefaultOutput)
+				std::cout << "This is the default output device.\n";
+			else
+				std::cout << "This is NOT the default output device.\n";
+			if (info.isDefaultInput)
+				std::cout << "This is the default input device.\n";
+			else
+				std::cout << "This is NOT the default input device.\n";
+			if (info.nativeFormats == 0)
+				std::cout << "No natively supported data formats(?)!";
+			else {
+				std::cout << "Natively supported data formats:\n";
+				if (info.nativeFormats & RTAUDIO_SINT8)
+					std::cout << "  8-bit int\n";
+				if (info.nativeFormats & RTAUDIO_SINT16)
+					std::cout << "  16-bit int\n";
+				if (info.nativeFormats & RTAUDIO_SINT24)
+					std::cout << "  24-bit int\n";
+				if (info.nativeFormats & RTAUDIO_SINT32)
+					std::cout << "  32-bit int\n";
+				if (info.nativeFormats & RTAUDIO_FLOAT32)
+					std::cout << "  32-bit float\n";
+				if (info.nativeFormats & RTAUDIO_FLOAT64)
+					std::cout << "  64-bit float\n";
+			}
+			if (info.sampleRates.size() < 1)
+				std::cout << "No supported sample rates found!";
+			else {
+				std::cout << "Supported sample rates = ";
+				for (unsigned int j = 0; j < info.sampleRates.size(); j++)
+					std::cout << info.sampleRates[j] << " ";
+			}
+			std::cout << std::endl;
+		}
+	}
+	std::cout << std::endl;
+
+	//END OF TEST
+
+	/*
+	 RtAudio *audio = 0;
+	 try {
+	 audio = new RtAudio();
+	 } catch (RtAudioError &error) {
+	 error.printMessage();
+	 std::cout << "Error initializing RtAudio";
+	 exit(EXIT_FAILURE);
+	 }
+	 */
 
 }
 
